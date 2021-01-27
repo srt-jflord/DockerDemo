88bb9de14845d7c659422c939cef36a1a6a8a7c7
Jean Lord <jlord@silkroadtech.com>
2021-01-27 07:15:28 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Docker Scale Demo
+A Simple ASP Core application that displays the container ID for a scaled service
+
+`docker-compose up -d`
+
+`docker-compose scale webapi=3`
---
+++ WebAPI/Controllers/HostNameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class HostNameController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return  Environment.MachineName;
+        }
+
+        
+
+        
+    }
+}
